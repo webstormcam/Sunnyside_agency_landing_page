05aca001821b8107eb742e1fdbb5e3044384937a
Cameron Chandler <webstormcam@gmail.com>
2021-10-26 07:55:55 -0400

    Adjusted the function
    Made it so it activated on the first click :)

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function myFunction(){
     let x = document.getElementById('small_triangle')
     let y = document.getElementById('links_small');
-    if(y.style.display === 'none'){
-        y.style.display = 'flex'
-        x.style.display ='block'
-    } else{
-        y.style.display ='none'
+    if(y.style.display === 'flex'){
+        y.style.display = 'none'
         x.style.display ='none'
+    } else{
+        y.style.display ='flex'
+        x.style.display ='block'
     }
 }
 
